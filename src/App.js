03055d4df7d434e7c483515b444fe6e6304badc0
yashi <yashiparikh1999@gmail.com>
2020-10-18 13:58:25 +0530

    adding files and folder.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+ import React from 'react';
 import './App.css';
-
+import{Layout , Header , Navigation  , Content} from 'react-mdl'
+import Main from './components/main';
+import {Link} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div >
+    <Layout  fixedHeader>
+        <Header className="header" title={<span><span style={{ color: '#ddd' }}></span><strong><a href="http://localhost:3000/">MY PORTFOLIO</a></strong></span>}>
+            <Navigation>
+                <Link to="/resume">Resume</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </Navigation>
+        </Header>
+        
+        <Content>
+            <div className="page-content">
+            <Main/>
+            </div>
+        </Content>
+    </Layout>
+</div>
+    )
 }
 
 export default App;
